94d3ec6de00b95c323bd445a1806ac16dc39d6d9
kyuuurina <17204442@siswa.um.edu.my>
2023-08-09 22:12:59 +0800

    Changed Sign In form title

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,7 +1,6 @@
 import { useUser } from "@supabase/auth-helpers-react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import Image from "next/image";
 
 import { AuthWrapper } from "~/components/auth/AuthWrapper";
 import { SignInForm } from "~/components/auth/SignInForm";
@@ -16,18 +15,11 @@ const SignIn: NextPage = () => {
   }
 
   return (
-    <AuthWrapper type="signin">
-      <div className="relative hidden w-0 flex-1 lg:block">
-        <Image
-          src="/Scholarspace.png"
-          width={500}
-          height={500}
-          alt="registration landing page"
-        />
-      </div>
-
-      <SignInForm />
-    </AuthWrapper>
+    <main className="mx-auto flex flex-col items-center justify-center bg-purple-100 px-6 py-8 md:h-screen lg:py-0">
+      <AuthWrapper type="signin">
+        <SignInForm />
+      </AuthWrapper>
+    </main>
   );
 };
 
