32e7822e51c4c10958d2b2f207a9455475c2d1bd
kyuuurina <17204442@siswa.um.edu.my>
2023-08-12 17:46:20 +0800

    extend tailwind color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-bg": "#F2F2FD",
+        "purple-text": "#14134A",
+        success: "#00C48C",
+        danger: "#FF3D71",
+        "purple-accent-1": "#6E6AF7",
+        "purple-accent-2": "#8F8CF1",
+        "blue-accent": "#6AB4F7",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
