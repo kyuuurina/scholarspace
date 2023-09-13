595e48d9c7fbbf9dbd0904aaf2c18de2f7973024
kyuuurina <17204442@siswa.um.edu.my>
2023-09-13 14:10:06 +0800

    test error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,34 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+interface ErrorPageProps {
+  statusCode?: number | null | undefined;
+}
+
+/**
+ * Rendered for 500 errors on both server and client. Used only in Production mode.
+ * @link https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
+ */
+const ErrorPage: NextPage<ErrorPageProps> = ({ statusCode }) => (
+  <>
+    <Head>
+      <title>Error</title>
+    </Head>
+    <div style={{ padding: 10 }}>
+      <h1>An error occurred</h1>
+      <p>
+        {statusCode
+          ? `A server-side ${statusCode} error occurred.`
+          : "A client-side error occurred."}
+      </p>
+    </div>
+  </>
+);
+
+ErrorPage.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+
+  return { statusCode };
+};
+
+export default ErrorPage;
