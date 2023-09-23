e230ebd71564ac9249a52e3c92b25370cbd3cf25
kyuuurina <17204442@siswa.um.edu.my>
2023-09-23 19:29:24 +0800

    added invite user through email search

--- src/components/button/PrimaryButton.tsx
+++ src/components/button/PrimaryButton.tsx
@@ -1,12 +1,20 @@
 type ButtonProps = {
   name: string;
   type?: "button" | "submit"; // Allow "button" and "submit" as valid values
+  disabled?: boolean;
 };
-const PrimaryButton: React.FC<ButtonProps> = ({ name, type = "button" }) => {
+const PrimaryButton: React.FC<ButtonProps> = ({
+  name,
+  type = "button",
+  disabled = false,
+}) => {
   return (
     <button
       type={type}
-      className="rounded-lg bg-purple-accent-1 px-3 py-2 text-center text-sm font-medium text-white hover:bg-purple-accent-2 focus:outline-none"
+      className={`rounded-lg bg-purple-accent-1 px-3 py-2 text-center text-sm font-medium text-white hover:bg-purple-accent-2 focus:outline-none
+      ${disabled ? "opacity-50" : ""}
+      `}
+      disabled={disabled}
     >
       {name}
     </button>
