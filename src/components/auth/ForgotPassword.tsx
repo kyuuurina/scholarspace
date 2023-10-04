0177b238164d106a7affb6eff3a49dadc6ce199b
Athirah <athirahlokman23@gmail.com>
2023-10-04 23:41:00 +0800

    forgot-password-1

---
+++ src/components/auth/ForgotPassword.tsx
@@ -0,0 +1,41 @@
+// ForgotPassword.tsx
+
+import React, { useState } from 'react';
+//import { forgotPassword } from 'src/server/api/trpc'; // Import tRPC function
+
+function ForgotPassword() {
+//   const [email, setEmail] = useState('');
+//   const [message, setMessage] = useState('');
+
+//   const handleForgotPassword = async () => {
+//     try {
+//       await forgotPassword.call({ email });
+//       setMessage('Password reset email sent. Please check your inbox.');
+//     } catch (error) {
+//       setMessage('An error occurred. Please try again later.');
+//     }
+//   };
+
+//   return (
+//     <div className="max-w-md mx-auto">
+//       <h1 className="text-3xl font-semibold mb-4">Forgot Password</h1>
+//       <p className="mb-4">Enter your email address to reset your password.</p>
+//       <input
+//         type="email"
+//         placeholder="Email"
+//         className="w-full p-2 mb-2 border rounded"
+//         value={email}
+//         onChange={(e) => setEmail(e.target.value)}
+//       />
+//       <button
+//         onClick={handleForgotPassword}
+//         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+//       >
+//         Reset Password
+//       </button>
+//       <p className="text-red-500 mt-2">{message}</p>
+//     </div>
+//   );
+// }
+}
+export default ForgotPassword;
