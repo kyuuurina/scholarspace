12a897212bd48676612ea49ea82c5fd76136a95c
kyuuurina <17204442@siswa.um.edu.my>
2023-10-14 16:25:17 +0800

    fixed avatarplaceholder import error

--- src/components/research-post/ResearchPostCard.tsx
+++ src/components/research-post/ResearchPostCard.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link";
-import { AvatarPlaceholder } from "../AvatarPlaceholder";
+import AvatarPlaceholder from "../AvatarPlaceholder";
 
 //Research Post Card wraps Avatar, Title, Details, Likes, Comments and Reposts
 export function ResearchPostCard() {
-    return (
-        <Link 
-        href="/research-post"
-        className="flex flex-col items-center justify-center w-full h-96 bg-gray-100 rounded-lg shadow-md cursor-pointer hover:bg-gray-200"
-        >
-            <div>
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold dark:text-white">
-                        Research Post Title
-                    </h5>
-                </div>
-            </div>
-        </Link>
-    );
+  return (
+    <Link
+      href="/research-post"
+      className="flex h-96 w-full cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-100 shadow-md hover:bg-gray-200"
+    >
+      <div>
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="mb-2 text-2xl font-bold dark:text-white">
+            Research Post Title
+          </h5>
+        </div>
+      </div>
+    </Link>
+  );
 }
