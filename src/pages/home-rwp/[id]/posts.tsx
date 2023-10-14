46054152b25ce757beadee343262d479f202e68a
kyuuurina <17204442@siswa.um.edu.my>
2023-10-14 16:32:18 +0800

    fixed pages export

--- src/pages/home-rwp/[id]/posts.tsx
+++ src/pages/home-rwp/[id]/posts.tsx
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {useForm} from "react-hook-form";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import { type ZodType, z } from "zod";
@@ -13,3 +13,18 @@ import type { NextPageWithLayout } from "~/pages/_app";
 //components
 import Layout from "~/components/layout/Layout";
 import Head from "~/components/layout/Head";
+
+const Posts: NextPageWithLayout = () => {
+  return <></>;
+};
+
+Posts.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <>
+      <Head title="Main Page" />
+      <Layout>{page}</Layout>
+    </>
+  );
+};
+
+export default Posts;
