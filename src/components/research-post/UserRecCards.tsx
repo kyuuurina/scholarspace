d3be8869bfd69040c1e88687d7dd0d97d1f8ae90
Athirah <athirahlokman23@gmail.com>
2023-11-22 08:20:42 +0800

    added link in user recommendation card component

--- src/components/research-post/UserRecCards.tsx
+++ src/components/research-post/UserRecCards.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-interface UserProfile {
-  id: number;
+interface UserRec {
+  id: string;
   name: string;
 }
 
-interface UserProfileCardProps {
-  users: UserProfile[];
+interface UserRecCardProps {
+  users: UserRec[];
 }
 
-const UserProfileCard: React.FC<UserProfileCardProps> = ({ users }) => {
+const UserRecCard: React.FC<UserRecCardProps> = ({ users }) => {
   const displayedUsers = users.slice(0, 10);
-
   return (
     <div className="p-4">
       <div className="bg-white shadow-md p-4 rounded-lg">
@@ -34,7 +34,9 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ users }) => {
                   className="rounded-full"
                 />
               </div>
-              <span className="text-base font-medium">{user.name}</span>
+              <Link href={`/user/${user.id}`}>
+                  <span className="text-base font-medium">{user.name}</span>
+              </Link>
             </li>
           ))}
         </ul>
@@ -43,4 +45,53 @@ const UserProfileCard: React.FC<UserProfileCardProps> = ({ users }) => {
   );
 };
 
-export default UserProfileCard;
+export default UserRecCard;
+
+// import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// interface UserRec {
+//   id: number;
+//   name: string;
+// }
+
+// interface UserRecCardProps {
+//   users: UserRec[];
+// }
+
+// //limit to 10 users recommendation
+// const UserRecCard: React.FC<UserRecCardProps> = ({ users }) => {
+//   const displayedUsers = users.slice(0, 10);
+
+//   return (
+//     <div className="p-4">
+//       <div className="bg-white shadow-md p-4 rounded-lg">
+//         <h2 className="text-lg font-semibold mb-4">People you may know...</h2>
+//         <ul>
+//           {displayedUsers.map((user, index) => (
+//             <li
+//               key={user.id}
+//               className={`flex items-center ${
+//                 index !== displayedUsers.length - 1 ? "mb-4" : ""
+//               }`}
+//             >
+//               <div className="w-12 h-12 mr-4">
+//                 <Image
+//                   src={`/url/to/avatar-image-for-${user.id}.jpg`} // Replace with the actual image path
+//                   alt={`${user.name}'s avatar`}
+//                   width={48}
+//                   height={48}
+//                   className="rounded-full"
+//                 />
+//               </div>
+//               <span className="text-base font-medium">{user.name}</span>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default UserRecCard;
