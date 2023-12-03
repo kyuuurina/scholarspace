052112ea1d3487d421cfd64a6de592ab4dee0e30
kyuuurina <17204442@siswa.um.edu.my>
2023-12-03 18:01:00 +0800

    Fixed Postcard Import

--- src/components/research-post/PostCard.tsx
+++ src/components/research-post/PostCard.tsx
@@ -4,13 +4,19 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
 import CategoryPills from "./CategoryPills";
-import { FiHeart, FiMessageSquare, FiRepeat, FiMoreHorizontal, FiEdit, FiTrash } from "react-icons/fi";
+import {
+  FiHeart,
+  FiMessageSquare,
+  FiRepeat,
+  FiMoreHorizontal,
+  FiEdit,
+  FiTrash,
+} from "react-icons/fi";
 import Comment from "~/components/research-post/Comment";
 
-import AvatarPlaceholder from "../AvatarPlaceholder";
+import AvatarPlaceholder from "~/components/avatar/AvatarPlaceholder";
 
 interface PostCardProps {
-
   title: string;
   category: string;
   author: string;
@@ -39,10 +45,10 @@ const PostCard: React.FC<PostCardProps> = ({
     setShowEditModal(true);
   };
 
- const handleDelete = () => {
+  const handleDelete = () => {
     setShowDropdown(false);
     setShowDeleteConfirmation(true);
-};
+  };
 
   const handleCancelEdit = () => {
     setShowEditModal(false);
@@ -59,28 +65,28 @@ const PostCard: React.FC<PostCardProps> = ({
   };
 
   return (
-    <div className="relative bg-white rounded-lg p-4 mb-4 shadow-md max-w-screen-md mx-auto">
-      <div className="absolute top-0 right-0">
+    <div className="relative mx-auto mb-4 max-w-screen-md rounded-lg bg-white p-4 shadow-md">
+      <div className="absolute right-0 top-0">
         <button
-          className="p-2 rounded-full hover:bg-gray-200 focus:outline-none"
+          className="rounded-full p-2 hover:bg-gray-200 focus:outline-none"
           onClick={handleToggleDropdown}
         >
           <FiMoreHorizontal size={24} />
         </button>
         {showDropdown && (
-          <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-200 rounded-md shadow-lg z-10">
+          <div className="absolute right-0 z-10 mt-2 w-40 rounded-md border border-gray-200 bg-white shadow-lg">
             <button
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+              className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
               onClick={handleEdit}
             >
-              <FiEdit size={16} className="inline-block mr-2" />
+              <FiEdit size={16} className="mr-2 inline-block" />
               Edit
             </button>
             <button
-              className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 w-full text-left"
+              className="block w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-gray-100"
               onClick={handleDelete}
             >
-              <FiTrash size={16} className="inline-block mr-2" />
+              <FiTrash size={16} className="mr-2 inline-block" />
               Delete
             </button>
           </div>
@@ -88,40 +94,40 @@ const PostCard: React.FC<PostCardProps> = ({
       </div>
 
       {/* content in the post card */}
-      <div className="flex items-center space-x-2 mb-4">
-        <span className="px-2 py-1 bg-blue-200 text-blue-800 rounded-full text-sm">
+      <div className="mb-4 flex items-center space-x-2">
+        <span className="rounded-full bg-blue-200 px-2 py-1 text-sm text-blue-800">
           {category}
         </span>
       </div>
-      <div className = "mt-2-flex items-center mt-4">
+      <div className="mt-2-flex mt-4 items-center">
         <h3 className="text-lg font-bold">{title}</h3>
-      </div>     
-      <div className= "mt-2-flex items-center mb-4">
+      </div>
+      <div className="mt-2-flex mb-4 items-center">
         <p className="text-sm">Created on: {created_at}</p>
       </div>
 
       <p className="mt-2 text-black">{description}</p>
 
-      <div className="mt-2-flex items-center mb-4">
+      <div className="mt-2-flex mb-4 items-center">
         <p className="mt-2 text-gray-500">Author: {author}</p>
       </div>
 
       {/* div of comment, likes and reshare */}
-      <div className="flex items-center mt-4">
+      <div className="mt-4 flex items-center">
         <button className="mr-2 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none">
-          <FiMessageSquare size={20} className="inline-block mr-2" />
+          <FiMessageSquare size={20} className="mr-2 inline-block" />
           Comment
         </button>
         <button className="mr-2 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none">
-          <FiHeart size={20} className="inline-block mr-2" />
+          <FiHeart size={20} className="mr-2 inline-block" />
           Like
         </button>
         <button className="flex items-center text-gray-500 hover:text-gray-700 focus:outline-none">
-          <FiRepeat size={20} className="inline-block mr-2" />
+          <FiRepeat size={20} className="mr-2 inline-block" />
           Reshare
         </button>
       </div>
-      
+
       <div className="mt-4">
         <Comment onAddComment={addComment} />
         <ul>
@@ -131,18 +137,18 @@ const PostCard: React.FC<PostCardProps> = ({
         </ul>
       </div>
       {showEditModal && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white p-4 rounded-md shadow-md">
-            <h3 className="text-lg font-bold mb-2">Edit Post</h3>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="rounded-md bg-white p-4 shadow-md">
+            <h3 className="mb-2 text-lg font-bold">Edit Post</h3>
             {/* Edit form fields go here */}
             <div className="flex justify-end">
               <button
-                className="px-4 py-2 mr-2 bg-gray-200 text-gray-700 rounded-md hover-bg-gray-300"
+                className="hover-bg-gray-300 mr-2 rounded-md bg-gray-200 px-4 py-2 text-gray-700"
                 onClick={handleCancelEdit}
               >
                 Cancel
               </button>
-              <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+              <button className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
                 Save
               </button>
             </div>
@@ -150,19 +156,19 @@ const PostCard: React.FC<PostCardProps> = ({
         </div>
       )}
       {showDeleteConfirmation && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="bg-white p-4 rounded-md shadow-md">
-            <h3 className="text-lg font-bold mb-2">Delete Confirmation</h3>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div className="rounded-md bg-white p-4 shadow-md">
+            <h3 className="mb-2 text-lg font-bold">Delete Confirmation</h3>
             <p>Are you sure you want to delete this post?</p>
             <div className="flex justify-end">
               <button
-                className="px-4 py-2 mr-2 bg-gray-200 text-gray-700 rounded-md hover-bg-gray-300"
+                className="hover-bg-gray-300 mr-2 rounded-md bg-gray-200 px-4 py-2 text-gray-700"
                 onClick={() => setShowDeleteConfirmation(false)}
               >
                 Cancel
               </button>
               <button
-                className="px-4 py-2 bg-red-500 text-white rounded-md hover-bg-red-600"
+                className="hover-bg-red-600 rounded-md bg-red-500 px-4 py-2 text-white"
                 onClick={handleConfirmDelete}
               >
                 Delete
