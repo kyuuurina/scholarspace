4fe8e31f98996a55ff184ed3bc0c1744ca10f1eb
Athirah <athirahlokman23@gmail.com>
2023-12-07 06:11:41 +0800

    frontend profile tabs colour

---
+++ src/pages/manage-profile/[id]/likedpost.tsx
@@ -0,0 +1,64 @@
+//Query all post created by user, if null, display "You have not created any post yet"
+
+import React from 'react';
+import {useState} from 'react';
+import {api} from "~/utils/api";
+import Image from 'next/image';
+import Link from 'next/link';
+
+//utils
+import { useRouterId } from "~/utils/routerId";
+import { useFetchProfile } from "~/utils/profile";
+
+// types
+import type { ReactElement } from "react";
+import type { NextPageWithLayout } from "~/pages/_app";
+
+import ErrorPage from "~/pages/error-page";
+//local components
+import Layout from "~/components/layout/Layout";
+import PageLoader from "~/components/layout/PageLoader";
+import LoadingSpinner from "~/components/LoadingSpinner";
+import PrimaryButton from "~/components/button/PrimaryButton";
+import AvatarPlaceholder from "~/components/AvatarPlaceholder";
+import { FaEdit } from 'react-icons/fa';
+
+//profile components
+import ProfileTabs from '~/components/profile/ProfileTabs';
+import UserProfileCard from "~/components/research-post/UserRecCards";
+import UserProfileForm from "~/components/profile/UserProfileForm";
+import EducationForm from "~/components/profile/EducationForm";
+import EducationCard from "~/components/profile/EducationCard";
+import Head from 'next/head';
+
+
+const LikedPost: NextPageWithLayout = () => {
+  const id = useRouterId();
+  const {
+    name,
+    about_me,
+    skills,
+    research_interest,
+    isLoading,
+    error
+  } = useFetchProfile();
+
+  return (
+    <>
+      {/* <Head title={name} /> */}
+      {/* <PageLoader isLoading={isLoading} errorMsg={error?.message}> */}
+      <ProfileTabs />
+      {/* </PageLoader> */}
+    </>
+  );
+};
+
+LikedPost.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <>
+      <Layout>{page}</Layout>
+    </>
+  );
+};
+
+export default LikedPost;
