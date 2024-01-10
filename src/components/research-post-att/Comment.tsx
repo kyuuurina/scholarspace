22f9997077b93afcda1aeca520b9233284fa7c1d
Athirah <athirahlokman23@gmail.com>
2024-01-10 12:29:01 +0800

    post comment refetch

---
+++ src/components/research-post-att/Comment.tsx
@@ -0,0 +1,263 @@
+// import type { post_comments } from "@prisma/client";
+// import { api } from "~/utils/api";
+// import { FiMoreHorizontal } from "react-icons/fi";
+// import React, { useState, useEffect } from "react";
+// import { type ZodType, z } from "zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import FormErrorMessage from "~/components/FormErrorMessage";
+// import toast from "react-hot-toast";
+// import ErrorToast from "~/components/toast/ErrorToast";
+// import { TRPCClientError } from "@trpc/client";
+// import { FiMessageSquare } from "react-icons/fi";
+
+// type CommentProps = {
+//     comment: Comment;
+//     refetch: () => Promise<void>;
+//     canReply?: boolean;
+//   };
+
+//   const Comment: React.FC<CommentProps> = ({
+//     comment,
+//     refetch,
+//     canReply = false,
+//   }) => {
+//     const { user_id, created_at, id } = comment;
+//     const [showActions, setShowActions] = useState(false);
+//     const [isEditMode, setIsEditMode] = useState(false);
+//     const [isReplyMode, setIsReplyMode] = useState(false);
+  
+//     const editComment = api.comment.edit.useMutation();
+//     const createReplyComment = api.comment.createReply.useMutation();
+  
+//     // form schema
+//     const schema: ZodType<{ value: string }> = z.object({
+//       value: z.string().min(10, { message: "Comment is too short" }),
+//     });
+  
+//     // react-hook-form
+//     const {
+//       handleSubmit,
+//       setValue,
+//       reset,
+//       formState: { errors },
+//     } = useForm<{ value: string }>({
+//       resolver: zodResolver(schema),
+//       defaultValues: {
+//         value: comment.value ?? "",
+//       },
+//     });
+  
+//     useEffect(() => {
+//       if (comment.value !== undefined) {
+//         setValue("value", comment.value || "");
+//       }
+//     }, [comment.value, setValue]);
+  
+//     // get user from api
+//     const { data: user } = api.user.get.useQuery(
+//       { id: user_id },
+//       { enabled: !!user_id }
+//     );
+  
+//     const handleUpdateComment = async (formData: { value: string }) => {
+//       // Send the edit request to the server
+//       try {
+//         await editComment.mutateAsync({
+//           id,
+//           ...formData,
+//         });
+//         reset();
+//         await refetch();
+//         setIsEditMode(false);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+  
+//     const handleCreateReplyComment = async (formData: { value: string }) => {
+//       // Send the reply request to the server
+//       try {
+//         await createReplyComment.mutateAsync({
+//           value: formData.value,
+//           task_id: comment.task_id,
+//           parent_id: comment.id,
+//         });
+//         reset();
+//         await refetch();
+//         setIsReplyMode(false);
+//       } catch (error) {
+//         toast.custom(() => {
+//           if (error instanceof TRPCClientError) {
+//             return <ErrorToast message={error.message} />;
+//           } else {
+//             // Handle other types of errors or fallback to a default message
+//             return (
+//               <ErrorToast message="Error to create reply. Please try again later." />
+//             );
+//           }
+//         });
+//       }
+//     };
+  
+//     const ref = useClickAway(() => {
+//       setShowActions(false);
+//     });
+  
+//     // delete comment
+//     const deleteComment = api.comment.delete.useMutation();
+//     const handleDeleteComment = async () => {
+//       try {
+//         await deleteComment.mutateAsync({ id });
+//         await refetch();
+//       } catch (error) {
+//         toast.custom(() => {
+//           if (error instanceof TRPCClientError) {
+//             return <ErrorToast message={error.message} />;
+//           } else {
+//             // Handle other types of errors or fallback to a default message
+//             return (
+//               <ErrorToast message="Error to delete comment. Please try again later." />
+//             );
+//           }
+//         });
+//       }
+//     };
+//     return (
+//       <div className="mb-1 border-t border-gray-200 bg-white p-4 text-base dark:border-gray-700 dark:bg-gray-900">
+//         <div className="mb-2 flex items-center justify-between">
+//           <div className="flex items-center">
+//             <p className="mr-3 inline-flex items-center text-sm font-semibold text-gray-900 dark:text-white">
+//               {user && <Avatar avatar_url={user.avatar_url} email={user.email} />}
+//               <span className="ml-2">{user?.name}</span>
+//             </p>
+//             <p className="text-sm text-gray-600 dark:text-gray-400">
+//               <time
+//                 dateTime={created_at.toLocaleString()}
+//                 title={created_at.toLocaleString()}
+//               >
+//                 {new Date(created_at).toLocaleDateString("en-GB", {
+//                   year: "numeric",
+//                   month: "long",
+//                   day: "numeric",
+//                 })}
+//               </time>
+//             </p>
+//           </div>
+//           <div className="relative">
+//             <div
+//               className="fa-icons cursor-pointer rounded-md p-2"
+//               onClick={() => setShowActions(true)}
+//             >
+//               <FiMoreHorizontal />
+//             </div>
+//             {showActions && (
+//               <div
+//                 className="absolute left-0 top-full"
+//                 ref={ref as React.MutableRefObject<HTMLDivElement>}
+//               >
+//                 <CommentActions
+//                   onEditClick={() => {
+//                     setIsEditMode(true);
+//                     setShowActions(false);
+//                   }}
+//                   onDeleteClick={async () => {
+//                     await handleDeleteComment();
+//                   }}
+//                 />
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//         {isEditMode ? (
+//           <div>
+//             <TextEditor
+//               documentValue={comment.value}
+//               setDocumentValue={(value) => setValue("value", value)}
+//             />
+//             <div className="flex justify-between">
+//               <div>
+//                 {errors.value && <FormErrorMessage text={errors.value.message} />}
+//               </div>
+//               <div className="flex justify-end">
+//                 <button className="px-3" onClick={() => setIsEditMode(false)}>
+//                   Cancel
+//                 </button>
+//                 <form
+//                   onSubmit={async (e) => {
+//                     e.preventDefault();
+//                     await handleSubmit(handleUpdateComment)(e);
+//                   }}
+//                   autoComplete="off"
+//                 >
+//                   <button type="submit" className="btn btn-primary">
+//                     Save
+//                   </button>
+//                 </form>
+//               </div>
+//             </div>
+//           </div>
+//         ) : isReplyMode && canReply ? (
+//           <div>
+//             <div
+//               className="pb-2 font-normal"
+//               dangerouslySetInnerHTML={{
+//                 __html: comment.value || "Add a comment here....",
+//               }}
+//             />
+//             <TextEditor
+//               documentValue={""}
+//               setDocumentValue={(value) => setValue("value", value)}
+//             />
+//             <div className="flex justify-between">
+//               <div>
+//                 {errors.value && <FormErrorMessage text={errors.value.message} />}
+//               </div>
+//               <div className="flex justify-end">
+//                 <button className="px-3" onClick={() => setIsReplyMode(false)}>
+//                   Cancel
+//                 </button>
+//                 <form
+//                   onSubmit={async (e) => {
+//                     e.preventDefault();
+//                     await handleSubmit(handleCreateReplyComment)(e);
+//                   }}
+//                   autoComplete="off"
+//                 >
+//                   <button type="submit" className="btn btn-primary">
+//                     Reply
+//                   </button>
+//                 </form>
+//               </div>
+//             </div>
+//           </div>
+//         ) : (
+//           <div>
+//             <div
+//               className="pb-2 font-normal"
+//               dangerouslySetInnerHTML={{
+//                 __html: comment.value || "Add a comment here....",
+//               }}
+//             />
+//             <div className="mt-1 flex items-center justify-start space-x-3">
+//               {canReply && (
+//                 <div className="flex items-center">
+//                   <button
+//                     type="button"
+//                     className="flex items-center space-x-1 text-sm text-gray-500 hover:underline"
+//                     onClick={() => setIsReplyMode(true)}
+//                   >
+//                     <FiMessageSquare />
+//                     <span>Reply</span>
+//                   </button>
+//                 </div>
+//               )}
+//               <ReactionButton commentId={id} />
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   };
+  
+//   export default Comment;
