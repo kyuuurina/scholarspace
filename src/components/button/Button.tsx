3eddf7da11914b0de2c4309646a2bb430a6a807d
kyuuurina <17204442@siswa.um.edu.my>
2024-01-12 05:48:40 +0800

    01-01

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MoonLoader } from "react-spinners";
 
 type ButtonProps = {
   name: string;
@@ -6,6 +7,8 @@ type ButtonProps = {
   type?: "button" | "submit" | "reset";
   style?: "primary" | "secondary";
   visible?: boolean;
+  isLoading?: boolean;
+  disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -14,6 +17,8 @@ const Button: React.FC<ButtonProps> = ({
   type = "button",
   style = "primary",
   visible = true,
+  isLoading = false,
+  disabled = false,
 }) => {
   return (
     <button
@@ -24,8 +29,12 @@ const Button: React.FC<ButtonProps> = ({
           : "rounded-lg border border-purple-700 px-5 py-2 text-center text-sm font-medium text-purple-700 hover:bg-purple-100 "
       } ${visible ? "visible" : "invisible"}`}
       onClick={onClick}
+      disabled={isLoading || disabled}
     >
-      {name}
+      <div className="flex">
+        {name}
+        {isLoading && <MoonLoader size={20} color="#ffffff" />}
+      </div>
     </button>
   );
 };
