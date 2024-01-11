e9b9d02af63cb617a268a0f52b7a6ef815d3d598
kyuuurina <17204442@siswa.um.edu.my>
2024-01-11 20:09:17 +0800

    env

--- src/components/auth/ForgotPasswordForm.tsx
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import { supabase } from '~/utils/supabase';
-import PrimaryButton from '../button/PrimaryButton';
+import { useState } from "react";
+import { supabase } from "~/utils/supabase";
+import PrimaryButton from "../button/PrimaryButton";
 
 const ForgotPasswordForm: React.FC = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState(false);
 
   const handleResetPassword = async () => {
     try {
       const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'http://localhost:3000/auth/reset-password',
+        redirectTo:
+          "https://scholarspace-kyuuurina.vercel.app/auth/reset-password",
       });
       if (error) {
         setError(error.message);
@@ -19,22 +20,25 @@ const ForgotPasswordForm: React.FC = () => {
         setError(null);
       }
     } catch (error) {
-      console.error('Error resetting password:', error as Error);
-      setError('Error resetting password. Please try again.');
+      console.error("Error resetting password:", error as Error);
+      setError("Error resetting password. Please try again.");
     }
   };
 
   return (
-    <div className="bg-purple-100 h-screen flex items-center justify-center">
-      <div className="max-w-md mx-auto p-4 bg-white rounded-md shadow-md">
+    <div className="flex h-screen items-center justify-center bg-purple-100">
+      <div className="mx-auto max-w-md rounded-md bg-white p-4 shadow-md">
         <div className="mb-4">
           <img
             src="/scholarspace-forgot-password.png"
             alt="Forgot Password"
             className="mx-auto mb-4"
           />
-          <h1 className="text-3xl font-semibold mb-4">Reset Password</h1>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+          <h1 className="mb-4 text-3xl font-semibold">Reset Password</h1>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-600"
+          >
             Email
           </label>
           <input
@@ -43,11 +47,15 @@ const ForgotPasswordForm: React.FC = () => {
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md"
+            className="mt-1 w-full rounded-md border p-2"
           />
         </div>
         {error && <p className="text-red-500">{error}</p>}
-        {success && <p className="text-green-500">Password reset instructions sent to your email.</p>}
+        {success && (
+          <p className="text-green-500">
+            Password reset instructions sent to your email.
+          </p>
+        )}
         <div className="mt-4">
           <PrimaryButton
             name="Reset Password"
