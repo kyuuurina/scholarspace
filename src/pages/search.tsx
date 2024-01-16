fe8ea4fb7f36cf044d8bb902c6341fa891d6bf3c
Athirah <athirahlokman23@gmail.com>
2024-01-16 10:12:15 +0800

    allow search profile

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -6,8 +6,10 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import SearchPost from '~/components/research-post/SearchPost';
 import Post from '~/components/research-post/Post';
+import MiniUserCard from "~/components/profile/MiniUserCard";
 import { api } from '~/utils/api';
 import { useFetchSearchResults } from '~/utils/researchpost';
+import { useFetchCombinedSearchResults } from "~/utils/search";
 import BackButton from '~/components/search/BackButton';
 import { useQuery } from "@tanstack/react-query";
 
@@ -28,6 +30,12 @@ interface Post {
   created_at: Date;
 }
 
+interface OtherUser {
+  profile_id: string;
+  name: string;
+  collab_status: string;
+}
+
 const SearchPage: NextPageWithLayout = () => {
   const supabase = useSupabaseClient();
   const session = useSession();
@@ -35,7 +43,7 @@ const SearchPage: NextPageWithLayout = () => {
   const query = router.query.query as string;
 
   // Call the hook directly within the component body
-  const { searchPostResults, isLoading, error } = useFetchSearchResults(query);
+  const { combinedResults, isLoading, error } = useFetchCombinedSearchResults(query);
 
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [currentPostId, setCurrentPostId] = useState<string | null>(null);
@@ -68,17 +76,22 @@ const SearchPage: NextPageWithLayout = () => {
       {isLoading && <p>Loading...</p>}
       {error && <p>Error fetching search results</p>}
 
-      {searchPostResults.length === 0 && !isLoading && !error && (
+      {combinedResults.length === 0 && !isLoading && !error && (
         <p>No matching results found.</p>
       )}
 
-        {searchPostResults.map((post, index) => (
-        <div key={post.post_id} className={`mb-12 ${index !== searchPostResults.length - 1 ? 'mb-12' : ''}`}>
-           <Post
-              post={post}
-              onEditClick={() => handleEditClick(post.post_id)}
+{combinedResults.map((result, index) => (
+        <div key={index} className={`mb-12 ${index !== combinedResults.length - 1 ? 'mb-12' : ''}`}>
+          {result.type === 'post' && (
+            <Post
+              post={result.data as Post}
+              onEditClick={() => handleEditClick((result.data as Post).post_id)}
               refetch={refetchPost}
             />
+          )}
+          {result.type === 'profile' && (
+            <MiniUserCard otherUser={result.data as OtherUser} />
+          )}
         </div>
       ))}
     </div>
