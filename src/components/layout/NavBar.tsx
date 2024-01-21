fd314d178b03c9f9ded551f94490bbc6daa15c93
kyuuurina <17204442@siswa.um.edu.my>
2024-01-22 05:21:33 +0800

    add workspace and project navigation on navbar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
 import { useState, useRef, useEffect } from "react";
 import type { profile } from "@prisma/client";
 import {
@@ -13,13 +16,30 @@ import "@knocklabs/react-notification-feed/dist/index.css";
 import AvatarPlaceholder from "../avatar/AvatarPlaceholder";
 import SignoutButton from "../auth/SignoutButton";
 
+const WorkspaceModal = dynamic(() => import("../workspace/WorkspaceModal"), {
+  loading: () => null,
+});
+
+const WorkspaceNavButton = dynamic(
+  () => import("../workspace/WorkspaceNavButton"),
+  {
+    loading: () => null,
+  }
+);
+
+const ProjectNavButton = dynamic(() => import("../project/ProjectNavButton"), {
+  loading: () => null,
+});
+
 type NavbarProps = {
   toggleSidebar: () => void;
   profile: profile;
 };
 
 const NavBar: React.FC<NavbarProps> = ({ toggleSidebar, profile }) => {
+  const router = useRouter();
   const [isVisible, setIsVisible] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const notifButtonRef = useRef(null);
 
   // constants for overlay
@@ -53,99 +73,113 @@ const NavBar: React.FC<NavbarProps> = ({ toggleSidebar, profile }) => {
     avatarUrl = `https://ighnwriityuokisyadjb.supabase.co/storage/v1/object/public/avatar/${profile.avatar_url}`;
   }
 
-  return (
-    <nav className="w-full border-b bg-white">
-      <div className="mx-2 flex flex-wrap items-center justify-between p-3">
-        <div className="flex gap-x-1">
-          <span className="cursor-pointer sm:hidden" onClick={toggleSidebar}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-purple-accent-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </span>
-          <Link href="/" className="flex items-center">
-            <Image
-              src="/scholarspace-logo-x1.png"
-              width={100}
-              height={100}
-              alt="Scholarspace logo"
-            />
-          </Link>
-        </div>
+  const isWorkspacePage1 = router.pathname.startsWith("/workspace");
+  const isProjectPage = router.pathname.startsWith("/project");
+  const isWorkspacePage = isWorkspacePage1 || isProjectPage;
 
-        <div className="flex items-center gap-x-4 text-white">
-          {/* Notifications */}
-          <KnockFeedProvider
-            apiKey={process.env.KNOCK_PUBLIC_API_KEY || ""}
-            feedId={process.env.KNOCK_FEED_ID || ""}
-            userId={profile.user_id}
-          >
-            <>
-              <NotificationIconButton
-                ref={notifButtonRef}
-                onClick={() => setIsVisible(!isVisible)}
-              />
-              <NotificationFeedPopover
-                buttonRef={notifButtonRef}
-                isVisible={isVisible}
-                onClose={() => setIsVisible(false)}
-              />
-            </>
-          </KnockFeedProvider>
-          {/* User Menu  */}
-          <div>
-            <div className="cursor-pointer" onClick={toggleUserOverlay}>
-              {avatarUrl ? (
-                <Image
-                  src={avatarUrl}
-                  width={32}
-                  height={32}
-                  alt="User avatar"
-                  loading="lazy"
+  return (
+    <>
+      <WorkspaceModal
+        openModal={modalIsOpen}
+        onClick={() => setModalIsOpen(false)}
+      />
+      <nav className="w-full border-b bg-white">
+        <div className="mx-2 flex flex-wrap items-center justify-between p-3">
+          <div className="flex gap-x-1">
+            <span className="cursor-pointer sm:hidden" onClick={toggleSidebar}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-purple-accent-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h8m-8 6h16"
                 />
-              ) : (
-                <div className="h-9 w-9">
-                  <AvatarPlaceholder name={profile.name} />
+              </svg>
+            </span>
+            <Link href="/" className="flex items-center">
+              <Image
+                src="/scholarspace-logo-x1.png"
+                width={100}
+                height={100}
+                alt="Scholarspace logo"
+              />
+            </Link>
+            {isWorkspacePage && (
+              <WorkspaceNavButton onClick={() => setModalIsOpen(true)} />
+            )}
+            {isProjectPage && <ProjectNavButton />}
+          </div>
+
+          <div className="flex items-center gap-x-4 text-white">
+            {/* Notifications */}
+            <KnockFeedProvider
+              apiKey={process.env.KNOCK_PUBLIC_API_KEY || ""}
+              feedId={process.env.KNOCK_FEED_ID || ""}
+              userId={profile.user_id}
+            >
+              <>
+                <NotificationIconButton
+                  ref={notifButtonRef}
+                  onClick={() => setIsVisible(!isVisible)}
+                />
+                <NotificationFeedPopover
+                  buttonRef={notifButtonRef}
+                  isVisible={isVisible}
+                  onClose={() => setIsVisible(false)}
+                />
+              </>
+            </KnockFeedProvider>
+            {/* User Menu  */}
+            <div>
+              <div className="cursor-pointer" onClick={toggleUserOverlay}>
+                {avatarUrl ? (
+                  <Image
+                    src={avatarUrl}
+                    width={32}
+                    height={32}
+                    alt="User avatar"
+                    loading="lazy"
+                  />
+                ) : (
+                  <div className="h-9 w-9">
+                    <AvatarPlaceholder name={profile.name} />
+                  </div>
+                )}
+              </div>
+              {/* Display user information */}
+              {isUserOverlayVisible && (
+                <div
+                  ref={overlayRef}
+                  className="min-w-44 absolute right-5 z-10 divide-y divide-gray-100 rounded-lg bg-white shadow"
+                >
+                  <div className="px-4 py-3 text-sm text-gray-900">
+                    <div>{profile.name}</div>
+                  </div>
+                  <ul className="py-2 text-sm text-gray-700">
+                    <li className="px-5 py-3 hover:bg-gray-100">
+                      <p>Manage Account</p>
+                    </li>
+                    <li
+                      className="px-5 py-3 hover:bg-gray-100"
+                      onClick={() => setIsUserOverlayVisible(false)}
+                    >
+                      <Link href="/settings">Settings</Link>
+                    </li>
+                  </ul>
+                  <SignoutButton />
                 </div>
               )}
             </div>
-            {/* Display user information */}
-            {isUserOverlayVisible && (
-              <div
-                ref={overlayRef}
-                className="min-w-44 absolute right-5 z-10 divide-y divide-gray-100 rounded-lg bg-white shadow"
-              >
-                <div className="px-4 py-3 text-sm text-gray-900">
-                  <div>{profile.name}</div>
-                </div>
-                <ul className="py-2 text-sm text-gray-700">
-                  <li className="px-5 py-3 hover:bg-gray-100">
-                    <p>Manage Account</p>
-                  </li>
-                  <li
-                    className="px-5 py-3 hover:bg-gray-100"
-                    onClick={() => setIsUserOverlayVisible(false)}
-                  >
-                    <Link href="/settings">Settings</Link>
-                  </li>
-                </ul>
-                <SignoutButton />
-              </div>
-            )}
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
