7e84a1fdc50fe7d2fcd216ef5925b06e5deca7dc
Athirah <athirahlokman23@gmail.com>
2024-01-23 19:36:35 +0800

    fix error chatlayout props

--- src/pages/chat/message/[id].tsx
+++ src/pages/chat/message/[id].tsx
@@ -9,16 +9,23 @@ import { useUser } from '@supabase/auth-helpers-react';
 import { useFetchChatMessages } from '~/utils/chatmessage';
 import { useFetchChatList } from '~/utils/chatmessage';
 import ChatLayout from '~/components/chat/ChatLayout';
+import {api} from "~/utils/api";
 
 
 const ChatMessagePage: NextPageWithLayout = () => {
 
-    const user = useUser();
-    const userId = user?.id ?? '';
+  const user = useUser();
+  const userId = user?.id ?? '';
   const router = useRouter();
   const { id } = router.query;
   const { chatList, isLoadingChatList, errorChatList } = useFetchChatList(userId);
   const chatId = typeof id === 'string' ? parseInt(id, 10) : null;
+
+  // Fetch messages
+  const messagesQuery = api.chat.getChatMessages.useQuery(
+    { chat_id: (chatList[0]?.chat_id || 0) },
+    { enabled: !!chatList[0]?.chat_id }
+  );
 
   // Ensure chatId is never null, provide a default value of 0
   const { chatMessages, isLoadingChatMessages, errorChatMessages } = useFetchChatMessages(chatId || 0);
@@ -51,6 +58,10 @@ const ChatMessagePage: NextPageWithLayout = () => {
     onChatSelect={handleChatSelect}
     selectedChatId={selectedChatId}
     chatMessages={[]}
+    refetch={async () => {
+      await messagesQuery.refetch();
+    }}
+
     >
       {/* Conditionally render an error message when chatId is null */}
       {!chatId && <p>Error in fetching messages.</p>}
